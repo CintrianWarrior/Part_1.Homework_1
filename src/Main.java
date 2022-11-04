7c3187453905168c01c6844d63056d744900a5c4
CintrianWarrior <velegoj2@yandex.ru>
2022-11-04 13:47:46 +0200

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Homework 1");
 
-        Human firstHuman = new Human(1988, "Максим", "Минск");
-        Human secondHuman = new Human(1993, "Аня", "Москва");
-        Human thirdHuman = new Human(1992, "Катя", "Калининград");
-        Human fourthHuman = new Human(1995, "Артем", "Москва");
+        Human firstHuman = new Human(1988, "Максим", "Минск", "бренд-менеджер");
+        Human secondHuman = new Human(1993, "Аня", "Москва", "методист образовательных программ");
+        Human thirdHuman = new Human(1992, "Катя", "Калининград", "продакт-менеджер");
+        Human fourthHuman = new Human(1995, "Артем", "Москва", "директор по развитию бизнеса");
         System.out.println(firstHuman);
         System.out.println(secondHuman);
         System.out.println(thirdHuman);
